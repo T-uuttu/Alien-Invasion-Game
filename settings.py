dd1c5cd162da80f2db3176643cba90b751062f38
Timo Uuttu <timo.uuttu@students.turkuamk.fi>
2022-02-24 19:57:24 +0200

    first commit

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings():
+    """ A class to store all settings for Alien Invasion Part Deux. """
+
+    def __init__(self):
+        """ Initialize the game's settings. """
+        # Screen settings
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (70, 30, 175)
+
+        # Ship settings
+        self.ship_speed = 1.5
+        
