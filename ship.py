bd277794d0813716df59bf3ff8b5c1bb95ade13f
Timo Uuttu <timo.uuttu@students.turkuamk.fi>
2022-02-25 16:48:09 +0200

    Added code so the ship moves within edges of the screen.

--- ship.py
+++ ship.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Ship:
     """ A class to manage the ship. """
 
@@ -18,22 +19,24 @@ class Ship:
 
         # Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
-        
+
         # Movement flags
         self.moving_right = False
         self.moving_left = False
-    
+
     def update(self):
         """ Update the ship's position based on the movement flag. """
         # Update the ship's x value, not the rect.
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-        
+
         # Update rect object from self.x
         self.rect.x = self.x
-    
+
     def blitme(self):
         """ Draw the ship at its current location. """
         self.screen.blit(self.image, self.rect)
+
+        # Test comment
